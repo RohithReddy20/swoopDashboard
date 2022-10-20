224f4d3533cc11e0c6cb0e780100873f65ca5ce3
Rohith Reddy <rohithreddyrohi18@gmail.com>
2022-10-20 19:58:24 +0000

    added authentication

---
+++ swoopdashboard/src/hooks/useAuth.js
@@ -0,0 +1,72 @@
+import { createContext, useContext, useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocalStorage } from "./useLocalStorage";
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useLocalStorage("user", null);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // call this function when you want to authenticate the user
+  const login = async () => {
+    const data = {
+      username : "",
+      password : "",
+    }
+    await fetch("https://asia-south1-swoop-fc-prod.cloudfunctions.net/dashboard/login",{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data)
+    }).then((response) => response.json()).then(data => {
+      setUser({
+        userName: userName,
+        password: password ,
+        accessToken: data
+      })
+    navigate("/matches");
+    });
+  };
+
+  // call this function to sign out logged in user
+  const logout = () => {
+    setUser(null);
+    navigate("/", { replace: true });
+  };
+
+  const viewlog = async () => {
+    await fetch("https://asia-south1-swoop-fc-prod.cloudfunctions.net/dashboard/match/winnings/view-log",{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Thunder Client (http://www.thunderclient.com)',
+        'x-access-token': user?.accessToken
+      },
+      // body: JSON.stringify(data)
+    }).then((response) => response.json()).then(data => {console.log(data)}).catch(err => console.log(err));
+  }
+
+  const value = useMemo(
+    () => ({
+      user,
+      setUser,
+      userName,
+      setUserName,
+      password, 
+      setPassword,
+      login,
+      logout,
+      viewlog
+    }),
+    [user]
+  );
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
